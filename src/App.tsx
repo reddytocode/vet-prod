9f5f04ae59fc32dfdb551e02f87bb5da94aa2e25
Reddy Tintaya <aaabeeelooon@gmail.com>
2020-08-02 21:36:50 -0400

    test material button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "@material-ui/core";
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
     </div>
   );
 }
