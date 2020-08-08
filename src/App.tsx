adde9627244a79e9bd10d2b1a01e899f900b1066
Reddy Tintaya <aaabeeelooon@gmail.com>
2020-08-07 21:03:18 -0400

    map

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Button} from "@material-ui/core";
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "@material-ui/core";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+
+const position = [51.505, -0.09];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Button variant="contained" color="primary">
-        Primary
-      </Button>
-    </div>
+    <Map center={[51.505, -0.09]} zoom={13}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup.
+          <br />
+          Easily customizable.
+        </Popup>
+      </Marker>
+    </Map>
   );
 }
 
